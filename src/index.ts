2339cc0cca30773733e8632cf57754fee7caa2d5
Raghav Luthra <raghavluthra1@gmail.com>
2022-09-16 21:02:53 +0530

    boiler plate

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import "reflect-metadata";
+import { ApolloServer } from "apollo-server-express";
+import * as Express from "express";
+import { Resolver, buildSchema, Query } from "type-graphql";
+
+@Resolver()
+class HelloResolver {
+   @Query(() => String)
+   async hello() {
+      return "Hello World";
+   }
+}
+
+const main = async () => {
+   const schema = await buildSchema({
+      resolvers: [HelloResolver],
+   });
+
+   const apolloServer = new ApolloServer({ schema });
+
+   const app = Express();
+
+   await apolloServer.start();
+
+   apolloServer.applyMiddleware({ app });
+
+   app.listen(4000, () => {
+      console.log("server started on http://localhost:4000/graphql");
+   });
+};
+
+main();
